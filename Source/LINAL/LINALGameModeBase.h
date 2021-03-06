baffa3a67eab17e0d4120e9043621a3a61986860
Huseyin <huseyincaliskan32@gmail.com>
2021-03-06 15:23:25 +0100

    init project

---
+++ Source/LINAL/LINALGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LINALGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LINAL_API ALINALGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
