9bb0c0123635aff68dec4a5051c7ba3c7ad98b3c
Huseyin <huseyincaliskan32@gmail.com>
2021-03-07 18:21:19 +0100

    Added operators

--- Source/LINAL/Public/FSVector.h
+++ Source/LINAL/Public/FSVector.h
@@ -16,9 +16,23 @@ struct FSVector
 public:
 	float X, Y, Z;
 
-	FORCEINLINE FSVector(float X, float Y, float Z);
-	
 	FORCEINLINE FSVector();
+
+	FORCEINLINE FSVector(float X, float Y, float Z);
+
+public:
+	FORCEINLINE FSVector operator+(const FSVector& V) const;
+	
+	FORCEINLINE FSVector operator-(const FSVector& V) const;
+	
+	FORCEINLINE FSVector operator*(const FSVector& V) const;
+	
+	
+	FORCEINLINE FSVector operator-(const float& F) const;
+	
+	FORCEINLINE FSVector operator+(const float& F) const;
+	
+	FORCEINLINE FSVector operator*(const float& Scale) const;
 
 public:
 	static const FSVector Zero;
@@ -26,13 +40,13 @@ public:
 	static const FSVector Up;
 
 	static const FSVector Down;
-	
+
 	static const FSVector Right;
-	
+
 	static const FSVector Left;
 
 	static const FSVector Forward;
-	
+
 	static const FSVector Backward;
 };
 
@@ -50,10 +64,40 @@ const FSVector FSVector::Forward(1.0f, 0.0f, 0.f);
 
 const FSVector FSVector::Backward(-1.0f, 0.0f, 0.f);
 
+FORCEINLINE FSVector::FSVector(): X(0), Y(0), Z(0)
+{
+}
+
 FORCEINLINE FSVector::FSVector(const float X, const float Y, const float Z): X(X), Y(Y), Z(Z)
 {
 }
 
-FORCEINLINE FSVector::FSVector(): X(0), Y(0), Z(0)
+inline FSVector FSVector::operator+(const FSVector& V) const
 {
+	return FSVector(X + V.X, Y + V.Y, Z + V.Z);
+}
+
+inline FSVector FSVector::operator-(const FSVector& V) const
+{
+	return FSVector(X - V.X, Y - V.Y, Z - V.Z);
+}
+
+inline FSVector FSVector::operator*(const FSVector& V) const
+{
+	return FSVector(X * V.X, Y * V.Y, Z * V.Z);	
+}
+
+inline FSVector FSVector::operator-(const float& F) const
+{
+	return FSVector(X - F, Y - F, Z - F);
+}
+
+inline FSVector FSVector::operator+(const float& F) const
+{
+	return FSVector(X + F, Y + F, Z + F);
+}
+
+inline FSVector FSVector::operator*(const float& Scale) const
+{
+	return FSVector(X * Scale, Y * Scale, Z * Scale);
 }
